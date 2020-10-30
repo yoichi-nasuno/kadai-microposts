ea4714f9a38e95779e8b197b148c9fe7fbaf5737
yoich-nasuno <nyoichi223@gmail.com>
2020-10-30 07:38:14 +0000

    10/30/16:30 microposts/nodels/user.rb has_many :relationships~has_many :followersまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
 
   has_secure_password
   
-    has_many :microposts
+  has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
 
 end
