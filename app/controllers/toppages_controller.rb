80277157650aa614522be3fe906a7a2f6db7ed0a
yoich-nasuno <nyoichi223@gmail.com>
2020-10-31 09:44:59 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,8 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @micropost = current_user.microposts.build  # form_with 用
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page]).per(5)
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page]).per(5)
     end
-
   end
 end
